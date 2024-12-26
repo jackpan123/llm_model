662d7ac39a746d794cc891e8cb211f647238fd0d
JackPan <826647196@qq.com>
2024-12-26 16:29:26 +0800

    complete chat bot

---
+++ qwen/chatBot.py
@@ -0,0 +1,63 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import streamlit as st
+
+# 在侧边栏中创建一个标题和一个链接
+with st.sidebar:
+    st.markdown("## Qwen2.5 LLM")
+    "[开源大模型食用指南 self-llm](https://github.com/datawhalechina/self-llm.git)"
+    # 创建一个滑块，用于选择最大长度，范围在 0 到 8192 之间，默认值为 512（Qwen2.5 支持 128K 上下文，并能生成最多 8K tokens）
+    max_length = st.slider("max_length", 0, 8192, 512, step=1)
+
+# 创建一个标题和一个副标题
+st.title("💬 Qwen2.5 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by Self-LLM")
+
+# 定义模型路径
+mode_name_or_path =  "/home/jackpan/qwen/model/qwen/Qwen2___5-7B-Instruct"
+
+# 定义一个函数，用于获取模型和 tokenizer
+@st.cache_resource
+def get_model():
+    # 从预训练的模型中获取 tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(mode_name_or_path, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    # 从预训练的模型中获取模型，并设置模型参数
+    model = AutoModelForCausalLM.from_pretrained(mode_name_or_path, torch_dtype=torch.bfloat16,  device_map="auto")
+
+    return tokenizer, model
+
+# 加载 Qwen2.5 的 model 和 tokenizer
+tokenizer, model = get_model()
+
+# 如果 session_state 中没有 "messages"，则创建一个包含默认消息的列表
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "有什么可以帮您的？"}]
+
+# 遍历 session_state 中的所有消息，并显示在聊天界面上
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+# 如果用户在聊天输入框中输入了内容，则执行以下操作
+if prompt := st.chat_input():
+
+    # 在聊天界面上显示用户的输入
+    st.chat_message("user").write(prompt)
+
+    # 将用户输入添加到 session_state 中的 messages 列表中
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # 将对话输入模型，获得返回
+    input_ids = tokenizer.apply_chat_template(st.session_state.messages,tokenize=False,add_generation_prompt=True)
+    model_inputs = tokenizer([input_ids], return_tensors="pt").to('cuda')
+    generated_ids = model.generate(model_inputs.input_ids,max_new_tokens=max_length)
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    # 将模型的输出添加到 session_state 中的 messages 列表中
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    # 在聊天界面上显示模型的输出
+    st.chat_message("assistant").write(response)
+    # print(st.session_state) # 打印 session_state 调试
